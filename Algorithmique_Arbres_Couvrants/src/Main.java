4d9fe1f7823b9c3d128f028f47d4624c055ce12a
Théo Roton <theo.roton@atilf.fr>
2020-12-30 21:17:45 +0100

    Début rapport

--- Algorithmique_Arbres_Couvrants/src/Main.java
+++ Algorithmique_Arbres_Couvrants/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		Graph G;
 		
-		G = Graph.exampleG0();
-		//G = Graph.exampleG1();
+		//G = Graph.exampleG0();
+		G = Graph.exampleG1();
 		
 		//Kruskal k = new Kruskal(); k.arbreCouvrant(G);
 		//AldousBroder a = new AldousBroder(); a.arbreCouvrant(G);
-		Wilson w = new Wilson(); w.arbreCouvrant(G);
+		//Wilson w = new Wilson(); w.arbreCouvrant(G);
     	
     	Display d = new Display();
     	d.setImage(G.toImage());
